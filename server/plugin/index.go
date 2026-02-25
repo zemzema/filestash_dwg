3a51c90bff1bb30e749fd3adc45040e84386a238
zemzema <33085810+zemzema@users.noreply.github.com>
2026-02-25 10:58:41 +0100

    Update index.go

--- server/plugin/index.go
+++ server/plugin/index.go
@@ -25,7 +25,7 @@ import (
 	_ "github.com/mickael-kerjean/filestash/server/plugin/plg_security_svg"
 	_ "github.com/mickael-kerjean/filestash/server/plugin/plg_starter_http"
 	_ "github.com/mickael-kerjean/filestash/server/plugin/plg_video_transcoder"
-	- "github.com/zemzema/filestash_dwg/tree/master/server/plugin/plg_viewer_dwg"
+	- "github.com/zemzema/filestash_dwg/server/plugin/plg_viewer_dwg"
 )
 
 func init() {
